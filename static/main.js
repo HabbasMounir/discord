32767580b46723e8b4626ba0f7f15704e7eef3d0
Mounir habbas <Mounirawt@gmail.com>
2024-01-25 23:12:23 +0100

    ل

--- static/main.js
+++ static/main.js
@@ -1,15 +1,14 @@
 let hidden=document.querySelectorAll(".hidden")
-
-
 hidden.forEach(e =>visibilehandle(e) )
 
 document.addEventListener("scroll", ()=>hidden.forEach(e =>visibilehandle(e) ) )
 
 
-
   function isInView(element) {
+
     const ClientRect = element.getBoundingClientRect()
+    ؤرؤ
     let y = window.innerHeight / 2
     return  ClientRect.top >= 0 && y>=ClientRect.top
 }
-  function visibilehandle(element) {isInView(element)&&element.classList.add("visibile")}
+    function visibilehandle(element) {isInView(element)&&element.classList.add("visibile")}
